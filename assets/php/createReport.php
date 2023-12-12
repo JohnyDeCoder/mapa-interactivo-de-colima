356a54de6647d82a5a012840eac142d736d9dfbd
BrandonMoran11 <bmoran2@ucol.mx>
2023-12-11 19:40:30 -0600

    reportMap design

--- assets/php/createReport.php
+++ assets/php/createReport.php
@@ -1,14 +1,32 @@
 <?php
 require_once '../../vendor/autoload.php';
 
+$css = file_get_contents('../css/mpdf.css'); // Estilo CSS
+
+$style = '<style>' . $css . '</style>';
+
 // Recibir la tabla HTML enviada por POST
 $tablaHTML = $_POST['tablaAGEBs'];
 
 // Crear una nueva instancia de mPDF
-$mpdf = new \Mpdf\Mpdf();
+$mpdf = new \Mpdf\Mpdf([
+    'format' => 'Letter',
+    'orientation' => 'P',
+    'default_font_size' => 10,
+    'default_font' => 'Arial',
+    'debug' => true,
+]);
+
+// Establecer márgenes
+$mpdf->SetAutoPageBreak(true, 25);
+
+// Establecer pie de página
+$mpdf->setFooter('|pag {PAGENO} / {nbpg}|');
+
+$html = $style . $tablaHTML;
 
 // Agregar la tabla HTML al PDF
-$mpdf->WriteHTML($tablaHTML);
+$mpdf->WriteHTML($html);
 
 // Generar el PDF y enviarlo al navegador
 $mpdf->Output('reporte-AGEBs.pdf', 'I');
