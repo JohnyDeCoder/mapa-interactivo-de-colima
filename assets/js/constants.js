ea712190e6d3a9000cb04fa270b1a65eae0d7b44
ElReyAlfredo <jdelgado7@ucol.mx>
2023-12-14 21:48:06 -0600

    Add more features to the constants

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -24,6 +24,182 @@ export const LABELED_LIST = {
       label: "Población Femenina",
       unit: "hab.",
     },
+    P_0A2: {
+      label: "Población de 0 a 2 años",
+      unit: "hab",
+    },
+    P_0A2_F: {
+      label: "Población femenina de 0 a 2 años",
+      unit: "hab",
+    },
+    P_0A2_M: {
+      label: "Población masculina de 0 a 2 años",
+      unit: "hab",
+    },
+    P_3YMAS: {
+      label: "Población de 3 años y más",
+      unit: "hab",
+    },
+    P_3YMAS_F: {
+      label: "Población femenina de 3 años y más",
+      unit: "hab",
+    },
+    P_3YMAS_M: {
+      label: "Población masculina de 3 años y más",
+      unit: "hab",
+    },
+    P_5YMAS: {
+      label: "Población de 5 años y más",
+      unit: "hab",
+    },
+    P_5YMAS_F: {
+      label: "Población femenina de 5 años y más",
+      unit: "hab",
+    },
+    P_5YMAS_M: {
+      label: "Población masculina de 5 años y más",
+      unit: "hab",
+    },
+    P_12YMAS: {
+      label: "Población de 12 años y más",
+      unit: "hab",
+    },
+    P_12YMAS_F: {
+      label: "Población femenina de 12 años y más",
+      unit: "hab",
+    },
+    P_12YMAS_M: {
+      label: "Población masculina de 12 años y más",
+      unit: "hab",
+    },
+    P_15YMAS: {
+      label: "Población de 15 años y más",
+      unit: "hab",
+    },
+    P_15YMAS_F: {
+      label: "Población femenina de 15 años y más",
+      unit: "hab",
+    },
+    P_15YMAS_M: {
+      label: "Población masculina de 15 años y más",
+      unit: "hab",
+    },
+    P_18YMAS: {
+      label: "Población de 18 años y más",
+      unit: "hab",
+    },
+    P_18YMAS_F: {
+      label: "Población femenina de 18 años y más",
+      unit: "hab",
+    },
+    P_18YMAS_M: {
+      label: "Población masculina de 18 años y más",
+      unit: "hab",
+    },
+    P_3A5: {
+      label: "Población de 3 a 5 años",
+      unit: "hab",
+    },
+    P_3A5_F: {
+      label: "Población femenina de 3 a 5 años",
+      unit: "hab",
+    },
+    P_3A5_M: {
+      label: "Población masculina de 3 a 5 años",
+      unit: "hab",
+    },
+    P_6A11: {
+      label: "Población de 6 a 11 años",
+      unit: "hab",
+    },
+    P_6A11_F: {
+      label: "Población femenina de 6 a 11 años",
+      unit: "hab",
+    },
+    P_6A11_M: {
+      label: "Población masculina de 6 a 11 años",
+      unit: "hab",
+    },
+    P_8A14: {
+      label: "Población de 8 a 14 años",
+      unit: "hab",
+    },
+    P_8A14_F: {
+      label: "Población femenina de 8 a 14 años",
+      unit: "hab",
+    },
+    P_8A14_M: {
+      label: "Población masculina de 8 a 14 años",
+      unit: "hab",
+    },
+    P_12A14: {
+      label: "Población de 12 a 14 años",
+      unit: "hab",
+    },
+    P_12A14_F: {
+      label: "Población femenina de 12 a 14 años",
+      unit: "hab",
+    },
+    P_12A14_M: {
+      label: "Población masculina de 12 a 14 años",
+      unit: "hab",
+    },
+    P_15A17: {
+      label: "Población de 15 a 17 años",
+      unit: "hab",
+    },
+    P_15A17_F: {
+      label: "Población femenina de 15 a 17 años",
+      unit: "hab",
+    },
+    P_15A17_M: {
+      label: "Población masculina de 15 a 17 años",
+      unit: "hab",
+    },
+    P_18A24: {
+      label: "Población de 18 a 24 años",
+      unit: "hab",
+    },
+    P_18A24_F: {
+      label: "Población femenina de 18 a 24 años",
+      unit: "hab",
+    },
+    P_18A24_M: {
+      label: "Población masculina de 18 a 24 años",
+      unit: "hab",
+    },
+    P_15A49_F: {
+      label: "Población femenina de 15 a 49 años",
+      unit: "hab",
+    },
+    P_60YMAS: {
+      label: "Población de 60 años y más",
+      unit: "hab",
+    },
+    P_60YMAS_F: {
+      label: "Población femenina de 60 años y más",
+      unit: "hab",
+    },
+    P_60YMAS_M: {
+      label: "Población masculina de 60 años y más",
+      unit: "hab",
+    },
+    REL_H_M: {
+      label: "Relación hombres-mujeres",
+      unit: "hab",
+    },
+    POB0_14: {
+      label: "Población de 0 a 14 años",
+      unit: "hab",
+    },
+    POB15_64: {
+      label: "Población de 15 a 64 años",
+      unit: "hab",
+    },
+    POB65_MAS: {
+      label: "Población de 65 años y más",
+      unit: "hab",
+    },
   },
   // Lista de capas de AGEBs
   AGEBS_LIST = {
